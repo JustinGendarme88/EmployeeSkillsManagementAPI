cf9a25d361bcb36d077defbde5b96961b0914c96
Justin Gendarme <justingendarme88@gmail.com>
2026-07-14 11:34:44 +0200

    Add validation to EmployeeSkill DTOs

--- EmployeeSkillsManagement.Api/DTOs/CreateEmployeeSkillDto.cs
+++ EmployeeSkillsManagement.Api/DTOs/CreateEmployeeSkillDto.cs
@@ -1,10 +1,15 @@
-﻿namespace EmployeeSkillsManagement.Api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeSkillsManagement.Api.DTOs;
 
 public class CreateEmployeeSkillDto
 {
+    [Range(1, int.MaxValue, ErrorMessage = "EmployeeId must be greater than 0.")]
     public int EmployeeId { get; set; }
 
+    [Range(1, int.MaxValue, ErrorMessage = "SkillId must be greater than 0.")]
     public int SkillId { get; set; }
 
+    [Range(1, 5, ErrorMessage = "Proficiency level must be between 1 and 5.")]
     public int ProficiencyLevel { get; set; }
 }
--- EmployeeSkillsManagement.Api/DTOs/UpdateEmployeeSkillDto.cs
+++ EmployeeSkillsManagement.Api/DTOs/UpdateEmployeeSkillDto.cs
@@ -1,6 +1,9 @@
-﻿namespace EmployeeSkillsManagement.Api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeSkillsManagement.Api.DTOs;
 
 public class UpdateEmployeeSkillDto
 {
+    [Range(1, 5, ErrorMessage = "Proficiency level must be between 1 and 5.")]
     public int ProficiencyLevel { get; set; }
 }
